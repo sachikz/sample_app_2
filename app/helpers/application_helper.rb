b9e70ba1efda56a312e00577558d310c917aee14
sachikz <sachikz01@gmail.com>
2018-10-25 13:54:23 +0000

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
   
   #ページごとの完全なタイトルを返します
   def full_title(page_title = '')  #メソッド定義と、引数のデフォルト値設定
-    base_title = "Sample App"
+    base_title = "こども語録"
     if page_title.empty?
       base_title
     else
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,31 +3,31 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   
   def setup
-    @base_title = "Sample App"
-  end
-  
-  test "should get root" do
-    get root_url
-    assert_response :success
-    assert_select "title", "#{@base_title}"
+    @base_title = "こども語録"
   end
   
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
-    get static_pages_help_url
+    get help_path
     assert_response :success
-    assert_select "title", "Help | #{@base_title}"
+    assert_select "title", "ヘルプ | #{@base_title}"
   end
   
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
-    assert_select "title", "About | #{@base_title}"
+    assert_select "title", "こども語録とは？ | #{@base_title}"
+  end
+  
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "問い合わせ | #{@base_title}"
   end
 
 end
