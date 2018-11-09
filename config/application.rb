c0617209639927a983f13900c91b8a1666325ac4
sachikz <sachikz01@gmail.com>
2018-11-09 15:09:35 +0000

    iine3

--- config/application.rb
+++ config/application.rb
@@ -17,5 +17,8 @@ module SampleApp2
     
     # デフォルトのlocaleを日本語(:ja)にする
     config.i18n.default_locale = :ja
+    
+    #認証トークンをremoteフォームに埋め込む
+    config.action_view.embed_authenticity_token_in_remote_forms = true
   end
 end
